c1770ada23cd6684827773026b38ff9637e366e3
Sebastian Londoño <sebastianl14@hotmail.com>
2020-11-10 11:40:37 -0500

    Implementación de Status para cuando se presente un inconveniente o no conexión a internet.

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -25,16 +25,18 @@ import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
 
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status String
-    val status: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
@@ -54,16 +56,20 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsRealEstateProperties() {
 
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
             try {
                 var listResult = MarsApi.retrofitService.getProperties()
-                _status.value = "Success: ${listResult.size} Mars properties retrieved"
+                //_status.value = "Success: ${listResult.size} Mars properties retrieved"
+                _status.value = MarsApiStatus.DONE
 
                 if (listResult.size > 0){
                     _properties.value = listResult
                 }
 
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+                //_status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
 
